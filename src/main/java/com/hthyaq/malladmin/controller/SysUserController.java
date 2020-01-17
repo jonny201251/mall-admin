308ba897c76322e1e613cce15d9ae3044afddbcc
jonny201251 <jonny201251@163.com>
2020-01-17 13:18:24 +0800

    159厂

--- src/main/java/com/hthyaq/malladmin/controller/SysUserController.java
+++ src/main/java/com/hthyaq/malladmin/controller/SysUserController.java
@@ -39,7 +39,7 @@ public class SysUserController {
         return user;
     }
 
-    //登录
+    //前台登录
     @PostMapping("/sysUser/login")
     public boolean login(HttpSession httpSession, @RequestParam("username") String username, @RequestParam("password") String password) {
         //根据用户名查询用户
@@ -55,6 +55,23 @@ public class SysUserController {
         }
         //将用户放入session中
         httpSession.setAttribute("user", user);
+        return true;
+    }
+
+    //后台登录
+    @PostMapping("/user/login")
+    @ResponseResult
+    public boolean adminLogin(@RequestBody SysUser sysUser) {
+        String loginName=sysUser.getLoginName();
+        String loginPassword=sysUser.getLoginPassword();
+        List<SysUser> list = sysUserService.list(new QueryWrapper<SysUser>().eq("login_name", loginName));
+        if (list.size() != 1) {
+            throw new RuntimeException("用户名或密码错误");
+        }
+        SysUser user = list.get(0);
+        if (!StringUtils.equals(user.getLoginPassword(), UserCodecUtils.md5Hex(loginPassword, user.getSalt()))) {
+            throw new RuntimeException("密码错误!");
+        }
         return true;
     }
 
