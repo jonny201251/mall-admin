92bf367d4d3c470d931b9118a4c1b45b3a859d54
jonny201251 <jonny201251@163.com>
2020-01-17 12:29:11 +0800

    159厂

--- src/main/java/com/hthyaq/malladmin/controller/MoneyLimitController.java
+++ src/main/java/com/hthyaq/malladmin/controller/MoneyLimitController.java
@@ -1,9 +1,16 @@
 package com.hthyaq.malladmin.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hthyaq.malladmin.model.entity.MoneyLimit;
+import com.hthyaq.malladmin.model.entity.SysUser;
+import com.hthyaq.malladmin.service.MoneyLimitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -16,5 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/moneyLimit")
 public class MoneyLimitController {
+    @Autowired
+    MoneyLimitService moneyLimitService;
 
+    //根据userId查询出一季度的金额
+    @GetMapping("/getMoney")
+    public Double getMoney(HttpSession httpSession) {
+        SysUser user = (SysUser) httpSession.getAttribute("user");
+        Integer companyId = user.getCompanyId();
+        MoneyLimit moneyLimit = moneyLimitService.getOne(new QueryWrapper<MoneyLimit>().eq("company_id", companyId).eq("quarter", 0));
+        return moneyLimit == null ? 0.0 : moneyLimit.getMoney();
+    }
 }
