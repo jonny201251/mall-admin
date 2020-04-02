1cb6c96bb50ebd5347631580cbf270a2295b1e34
jonny201251 <jonny201251@163.com>
2020-04-02 11:15:41 +0800

    app结算

--- src/main/java/com/hthyaq/malladmin/controller/app/AppOrderController.java
+++ src/main/java/com/hthyaq/malladmin/controller/app/AppOrderController.java
@@ -1,13 +1,21 @@
 package com.hthyaq.malladmin.controller.app;
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.google.common.collect.Sets;
 import com.hthyaq.malladmin.common.annotation.ResponseResult;
 import com.hthyaq.malladmin.common.exception.MyExceptionNotCatch;
 import com.hthyaq.malladmin.model.bean.Cart;
+import com.hthyaq.malladmin.model.dto.OrderDTO;
+import com.hthyaq.malladmin.model.entity.OrderInfo;
+import com.hthyaq.malladmin.model.entity.OrderStatus;
 import com.hthyaq.malladmin.model.entity.SysUser;
 import com.hthyaq.malladmin.service.CartService;
+import com.hthyaq.malladmin.service.OrderInfoService;
+import com.hthyaq.malladmin.service.OrderStatusService;
 import com.hthyaq.malladmin.service.SysUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -19,10 +27,15 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/app/order")
 @ResponseResult
 public class AppOrderController {
+    @Autowired
+    private OrderInfoService orderInfoService;
+    @Autowired
+    private OrderStatusService orderStatusService;
     @Autowired
     private CartService cartService;
     @Autowired
@@ -53,6 +66,44 @@ public class AppOrderController {
                 .filter(cart -> skuSet.contains(cart.getSkuId()))
                 .collect(Collectors.toList());
         return carts;
+    }
+
+    //提交订单
+    @GetMapping("/create")
+    public List<Long> createOrder(Integer userId, String skuIds) {
+
+        //取出登录用户
+        SysUser user = sysUserService.getById(userId);
+        //填充orderDTO
+        OrderDTO orderDTO = new OrderDTO();
+        try {
+            return orderInfoService.createOrder(user, orderDTO);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new MyExceptionNotCatch("[创建订单] 创建订单失败");
+        }
+    }
+
+    @GetMapping("/queryById")
+    public OrderInfo queryById(String orderId) {
+        return orderInfoService.queryById(orderId);
+    }
+
+    @GetMapping("/orderList")
+    public IPage<OrderInfo> orderList(Integer userId, Integer currentPage, Integer pageSize, String orderId, String companyId, String status) {
+        SysUser user = sysUserService.getById(userId);
+        IPage<OrderInfo> page = orderInfoService.getOrderList(user, currentPage, pageSize, orderId, companyId, status);
+        return page;
+    }
+
+    //取消订单
+    @GetMapping("/orderStatus")
+    public boolean orderCancel(String orderId, String status) {
+        boolean flag;
+        OrderStatus orderStatus = orderStatusService.getOne(new QueryWrapper<OrderStatus>().eq("order_id", orderId));
+        orderStatus.setStatus(Integer.parseInt(status));
+        flag = orderStatusService.updateById(orderStatus);
+        return flag;
     }
 
     @GetMapping("/delete")
